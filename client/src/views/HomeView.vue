be66ce77f71b2a27ad24ef75e164cba25bb84672
Leo Johriel <leojohrielrivera17@gmail.com>
2024-07-07 20:34:50 +0800

    added a dashboard

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,6 +1,30 @@
 <template>
-  <div class="home">
-    <h1>Hellow World!</h1>
+  <div class="row">
+    <div class="col-lg-12 mt-3">
+      <div class="card">
+        <div class="card-body">
+          <h4 class="header-title">DASHBOARD</h4>
+          <div class="clearfix"></div>
+          <hr />
+          <div class="row">
+            <div class="col-md-4 col-12 mb-2 item">
+              <div @click="goToBooks" class="card bg-primary bg-gradient shadow">
+                <div class="card-body">
+                  <h5 class="card-title text-center text-light">{{books}} TOTAL BOOKS</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4 col-12 mb-2 item">
+              <div @click="goToBorrowedBooks" class="card bg-primary bg-gradient shadow">
+                <div class="card-body">
+                  <h5 class="card-title text-center text-light">20 BORROWED BOOOKS</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,9 +33,35 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-
+  name: "HomeView",
+  components: {},
+  data() {
+    return {
+      books : 0,
+      borrowedBooks : 0
+    }
+  },
+  methods:{
+    goToBooks(){
+      this.$router.push('/books')
+    },
+    goToBorrowedBooks(){
+      this.$router.push('/books')
+    },
+    async countBooks(){
+      const res = await fetch('http://localhost:4000/api/dashboard')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created() {
+    this.books = await this.countBooks()
   }
-}
+};
 </script>
+
+<style scoped>
+.item:hover{
+  cursor: pointer;
+}
+</style>
--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const PORT = 4000
 
 //routes
 import booksRoute from './routes/booksRoute.js'
+import dashboardRoute from './routes/dashboardRoute.js'
 
 const app = express()
 
@@ -13,6 +14,7 @@ app.use(cors())
 
 //route middlewware
 app.use('/api/books', booksRoute)
+app.use('/api/dashboard', dashboardRoute)
 
 app.listen(PORT, () => {
     console.log(`listening to port ${PORT}`)
