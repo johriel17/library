feaec36f63e014298c93d54357ac62e93f00d294
Leo Johriel <leojohrielrivera17@gmail.com>
2024-07-07 18:48:51 +0800

    created pagination component and used it on Index view

---
+++ client/src/components/Pagination.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="row">
+    <div class="col-md-12">
+      <ul class="pagination justify-content-center">
+        <li :class="['page-item', pagination.currentPage === 1 ? 'disabled' : '']">
+          <a class="page-link" href="javascript:void(0)" @click="$emit('first-page', 1)"
+            ><sub>&laquo;&laquo;</sub></a
+          >
+        </li>
+        <li :class="['page-item', 'prevPage', pagination.currentPage === 1 ? 'disabled' : '']">
+          <a class="page-link" href="javascript:void(0)" @click="$emit('prev-page', pagination.currentPage - 1)">&laquo;</a>
+        </li>
+        <li ng-repeat="page in pages" class="page-item">
+          <a class="page-link" href="javascript:void(0)">{{pagination.currentPage}}</a>
+        </li>
+        <li :class="['page-item', 'nextPage', pagination.currentPage >= pagination.totalPages ? 'disabled' : '']">
+          <a class="page-link" href="javascript:void(0)" @click="$emit('next-page', pagination.currentPage + 1)">&raquo;</a>
+        </li>
+        <li :class="['page-item', pagination.currentPage >= pagination.totalPages ? 'disabled' : '']">
+          <a class="page-link" href="javascript:void(0)" @click="$emit('last-page', pagination.totalPages)"
+            ><sub>&raquo;&raquo;</sub>
+          </a>
+        </li>
+      </ul>
+      <div class="clearfix"></div>
+      <div class="text-center" ng-show="paginator.pageCount > 0">
+        <sup class="text-primary">Page {{pagination.currentPage}} of {{pagination.totalPages}}</sup>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'Pagination',
+    props : {
+        pagination : Object
+    }
+}
+</script>
--- client/src/views/books/Index.vue
+++ client/src/views/books/Index.vue
@@ -12,7 +12,7 @@
 
         </div>
         <div class="clearfix"></div><hr>
-        <div class="single-table mb-5">
+        <div class="single-table mb-2">
           <div class="table-responsive">
             <table class="table table-bordered text-center">
             <thead>
@@ -28,73 +28,49 @@
                 </tr>
             </thead>
             <tbody>
-      <tr :key="book.id" v-for="(book, index) in books">
-        <th class="text-center" scope="row">{{ index + 1 }}</th>
-        <td>{{ book.book_number }}</td>
-        <td>{{ book.title }}</td>
-        <td>{{ book.author }}</td>
-        <td>{{ book.publisher }}</td>
-        <td class="text-center">{{ book.year }}</td>
-        <td class="text-center">{{ book.copies }}</td>
-        <td class="text-center">
-          <div class="btn-group btn-group-xs">
-            <router-link
-              :to="`books/view/${book.id}`"
-              class="btn btn-success"
-              title="VIEW"
-              ><i class="fa fa-eye"></i
-            ></router-link>
+                <tr :key="book.id" v-for="(book, index) in books">
+                  <th class="text-center" scope="row">{{ (pagination.currentPage - 1 ) * 10 + index + 1 }}</th>
+                  <td>{{ book.book_number }}</td>
+                  <td>{{ book.title }}</td>
+                  <td>{{ book.author }}</td>
+                  <td>{{ book.publisher }}</td>
+                  <td class="text-center">{{ book.year }}</td>
+                  <td class="text-center">{{ book.copies }}</td>
+                  <td class="text-center">
+                    <div class="btn-group btn-group-xs">
+                      <router-link
+                        :to="`books/view/${book.id}`"
+                        class="btn btn-success"
+                        title="VIEW"
+                        ><i class="fa fa-eye"></i
+                      ></router-link>
 
-            <router-link
-              :to="`books/edit/${book.id}`"
-              class="btn btn-primary"
-              title="EDIT"
-              ><i class="fa fa-edit"></i
-            ></router-link>
+                      <router-link
+                        :to="`books/edit/${book.id}`"
+                        class="btn btn-primary"
+                        title="EDIT"
+                        ><i class="fa fa-edit"></i
+                      ></router-link>
 
-            <a
-              href="javascript:void(0)"
-              ng-click="remove(data)"
-              class="btn btn-danger"
-              title="DELETE"
-              ><i class="fa fa-trash"></i
-            ></a>
-          </div>
-        </td>
-      </tr>
-      <tr v-show="!books.length > 0">
-        <td colspan="12" class="fw-bold">NO AVAILABLE DATA</td>
-      </tr>
-    </tbody>
+                      <a
+                        href="javascript:void(0)"
+                        ng-click="remove(data)"
+                        class="btn btn-danger"
+                        title="DELETE"
+                        ><i class="fa fa-trash"></i
+                      ></a>
+                    </div>
+                  </td>
+                </tr>
+                <tr v-show="!books.length > 0">
+                  <td colspan="12" class="fw-bold">NO AVAILABLE DATA</td>
+                </tr>
+            </tbody>
             </table>
           </div>
         </div>
         <div class="clearfix"></div>
-        <!-- <div class="row">
-          <div class="col-md-12">
-            <ul class="pagination justify-content-center">
-              <li class="page-item">
-                <a class="page-link" href="javascript:void(0)" ng-click="load({ page: 1, search: searchTxt })"><sub>&laquo;&laquo;</sub></a>
-              </li>
-              <li class="page-item prevPage {{ !paginator.prevPage? 'disabled':'' }}">
-                <a class="page-link" href="javascript:void(0)" ng-click="load({ page: paginator.page - 1, search: searchTxt })">&laquo;</a>
-              </li>
-              <li ng-repeat="page in pages" class="page-item {{ paginator.page == page.number ? 'active':''}}" >
-                <a class="page-link" href="javascript:void(0)" class="text-center" ng-click="load({ page: page.number, search: searchTxt })">{{ page.number }}</a>
-              </li>
-              <li class="page-item nextPage {{ !paginator.nextPage? 'disabled':'' }}">
-                <a class="page-link" href="javascript:void(0)" ng-click="load({ page: paginator.page + 1, search: searchTxt })">&raquo;</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="javascript:void(0)" ng-click="load({ page: paginator.pageCount, search: searchTxt })"><sub>&raquo;&raquo;</sub> </a>
-              </li>
-            </ul>
-            <div class="clearfix"></div>
-            <div class="text-center" ng-show="paginator.pageCount > 0">
-              <sup class="text-primary">Page {{ paginator.pageCount > 0 ? paginator.page : 0 }} out of {{ paginator.pageCount }}</sup>
-            </div>
-          </div>
-        </div> -->
+        <Pagination :pagination="pagination" @next-page="changePage" @last-page="changePage" @prev-page="changePage" @first-page="changePage" />
       </div>
     </div>
   </div>
@@ -103,33 +79,45 @@
 </template>
 
 <script>
-import ButtonGroup from '../../components/ButtonGroup.vue'
-
+import ButtonGroup from '@/components/ButtonGroup.vue'
+import Pagination from '@/components/Pagination.vue'
 
 export default {
     name : 'Index',
     components: {
-        ButtonGroup
+        ButtonGroup,
+        Pagination
     },
     data() {
         return {
-            books : []
+            books : [],
+            pagination : {},
+            searchTxt : ''
         }
     },
     methods : {
         async fetchBooks() {
             const res = await fetch('http://localhost:4000/api/books')
             const data = await res.json()
-            return data
+            this.books = data.books
+            this.pagination = data.pagination
         },
         async search(searchTxt){
+          this.searchTxt = searchTxt
           const res = await fetch(`http://localhost:4000/api/books?search=${searchTxt}`)
           const data = await res.json()
-          this.books = data
-        }
+          this.books = data.books
+          this.pagination = data.pagination
+        },
+        async changePage(page){
+          const res = await fetch(`http://localhost:4000/api/books?search=${this.searchTxt}&page=${page}`)
+          const data = await res.json()
+          this.books = data.books
+          this.pagination = data.pagination
+        },
     },
     async created() {
-        this.books = await this.fetchBooks()
+         await this.fetchBooks()
     }
     
 }
