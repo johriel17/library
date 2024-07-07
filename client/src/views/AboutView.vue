3b1b0969c8e64dfca31f26b8e5ff080bb3ba5ab6
Leo Johriel <leojohrielrivera17@gmail.com>
2024-07-07 23:12:44 +0800

    changed about laayout

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="row">
+    <div class="col-lg-12 mt-3">
+      <div class="card">
+        <div class="card-body">
+          <h4 class="header-title">ABOUT</h4>
+          <div class="clearfix"></div>
+          <hr />
+          <div class="row">
+            <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</h4>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
