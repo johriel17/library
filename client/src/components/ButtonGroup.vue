2f7e27337e0b8f8bdf4e4634abdf4665c56b68c1
Leo Johriel <leojohrielrivera17@gmail.com>
2024-07-07 15:58:17 +0800

    added margin buttom

--- client/src/components/ButtonGroup.vue
+++ client/src/components/ButtonGroup.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="row">
-    <div class="col-md-8 d-flex align-items-center gap-2">
-      <router-link :to="addPath" class="btn btn-primary btn-min">
-        <i class="fa fa-plus"></i> {{ addLabel }}
-      </router-link>
-      <button type="button" class="btn btn-warning btn-min" @click="reload">
-        <i class="fa fa-refresh"></i> Reload
-      </button>
+    <div class="col-md-8 d-flex align-items-center gap-2 mb-2">
+        <router-link :to="addPath" class="btn btn-primary btn-min">
+            <i class="fa fa-plus"></i> {{ addLabel }}
+        </router-link>
+        <button type="button" class="btn btn-warning btn-min" @click="reload">
+            <i class="fa fa-refresh"></i> Reload
+        </button>
     </div>
     <div class="col-md-4 d-inline">
       <div class="input-group-prepend">
