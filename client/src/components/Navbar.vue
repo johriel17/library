7cc27710f5befab54b63cefc1936fb3d0a6086cd
Leo Johriel <leojohrielrivera17@gmail.com>
2024-07-08 09:30:39 +0800

    added a design when link is active

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -16,16 +16,16 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <router-link class="nav-link" to="/">Home</router-link>
+            <router-link :class="['nav-link', isActive('/') ? 'active' : '']" to="/">Home</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/books">Books</router-link>
+            <router-link :class="['nav-link', isActive('/books') ? 'active' : '']" to="/books">Books</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/borrowed-books">Borrowed Books</router-link>
+            <router-link :class="['nav-link', isActive('/borrowed-books') ? 'active' : '']" to="/borrowed-books">Borrowed Books</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/about">About</router-link>
+            <router-link :class="['nav-link', isActive('/about') ? 'active' : '']" to="/about">About</router-link>
           </li>
         </ul>
       </div>
@@ -35,13 +35,33 @@
 
 <script>
 export default {
-  name: "Navbar",
+  name: "Navbar", 
+  computed : {
+    currentPath() {
+      return this.$route.path
+    },
+    isActive() {
+      return (path) => {
+        if(path === '/'){
+          return this.currentPath === path
+        }
+        const regex = new RegExp(`^${path}`);
+        return regex.test(this.currentPath);
+      };
+    },
+  }
 };
 </script>
 
 <style scoped>
 .navbar {
   background-color: #e3f2fd;
+}
+
+.active{
+  text-decoration: underline double !important;
+
+  color:blue !important
 }
 
 </style>
