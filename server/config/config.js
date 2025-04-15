1f9a81fd5fca4468d58eac8f270609f7b8e9c0dc
Leo Johriel <leojohrielrivera17@gmail.com>
2025-04-15 13:10:09 +0800

    specify port

--- server/config/config.js
+++ server/config/config.js
@@ -8,21 +8,24 @@ const config = {
     password: process.env.DEV_PASSWORD,
     database: process.env.DEV_DATABASE,
     host: process.env.DEV_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: process.env.DEV_PORT,
   },
   test: {
     username: process.env.TEST_USERNAME,
     password: process.env.TEST_PASSWORD,
     database: process.env.TEST_DATABASE,
     host: process.env.TEST_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: process.env.TEST_PORT,
   },
   production: {
     username: process.env.PROD_USERNAME,
     password: process.env.PROD_PASSWORD,
     database: process.env.PROD_DATABASE,
     host: process.env.PROD_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: process.env.PROD_PORT,
   }
 };
 
--- server/config/database.js
+++ server/config/database.js
@@ -2,11 +2,12 @@ import { Sequelize } from 'sequelize';
 import config from './config.js';
 
 const env = process.env.NODE_ENV || 'development';
-const { database, username, password, host, dialect } = config[env];
+const { database, username, password, host, dialect, port } = config[env];
 
 const sequelize = new Sequelize(database, username, password, {
   host,
   dialect,
+  port,
 });
 
 export default sequelize;
